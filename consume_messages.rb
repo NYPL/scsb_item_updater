3a8039bd8669e5869c1f3d54d4434917306152cd
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-01-16 16:08:41 -0500

    add VERY basic long polling

--- consume_messages.rb
+++ consume_messages.rb
@@ -14,25 +14,13 @@ settings = YAML.load(ERB.new(File.read(path_to_settings)).result)
 # Configure SQS Client
 credentials = Aws::Credentials.new(settings['aws_key'], settings['aws_secret'])
 sqs_client  = Aws::SQS::Client.new(region: 'us-east-1', credentials: credentials)
+poller      = Aws::SQS::QueuePoller.new(settings['sqs_queue_url'], client: sqs_client)
 
-response = sqs_client.receive_message(queue_url: settings['sqs_queue_url'], attribute_names: ['All'])
+poll_options = {max_number_of_messages: 10, skip_delete: true, wait_time_seconds: settings['polling_interval_seconds'], attribute_names: ['All']}
 
-# Each message is an instance of:
-# https://docs.aws.amazon.com/sdkforruby/api/Aws/SQS/Types/Message.html
-response.messages.each do |message|
-  puts message.body
+poller.poll(poll_options) do |messages|
+  messages.each do |message|
+    puts "Message body: #{message.body} with attributes #{message.attributes} and user_attributes of #{message.message_attributes}\n"
+  end
+
 end
-
-
-# Polling is probably what we REALLY want to do although we need
-# to avoid the default configuration which deletes messages at the
-# end of the poll.
-
-# https://docs.aws.amazon.com/sdkforruby/api/Aws/SQS/QueuePoller.html
-# poller = Aws::SQS::QueuePoller.new(settings['sqs_queue_url'])
-#
-# poller.poll({max_number_of_messages: 10}) do |messages|
-#   messages.each do |message|
-#     puts "Message body: #{message.body}"
-#   end
-# end
