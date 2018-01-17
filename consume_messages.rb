27d4144c740a32a14cfdedc0f875777ed1f3de1a
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-01-17 12:37:28 -0500

    Add basic printing of barcode=>customerCode when we ingest a message

--- consume_messages.rb
+++ consume_messages.rb
@@ -1,4 +1,5 @@
 require 'aws-sdk'
+require File.join(__dir__, 'lib', 'barcode_to_customer_code_mapper')
 
 # Bring environment variables in ./config/.env into scope.
 # This is probably only used in development.
@@ -8,6 +9,7 @@ Dotenv.load(File.join('.', 'config', '.env'))
 # Create a hash that contains the configuarable variables
 require 'erb'
 require 'yaml'
+
 path_to_settings = File.join(__dir__, "config", "settings.yml")
 settings = YAML.load(ERB.new(File.read(path_to_settings)).result)
 
@@ -21,6 +23,8 @@ poll_options = {max_number_of_messages: 10, skip_delete: true, wait_time_seconds
 poller.poll(poll_options) do |messages|
   messages.each do |message|
     puts "Message body: #{message.body} with attributes #{message.attributes} and user_attributes of #{message.message_attributes}\n"
+    parsed_message = JSON.parse(message.body)
+    mapper = BarcodeToCustomerCodeMapper.new({barcodes: parsed_message['barcodes'], api_url:  settings['scsb_api_url'], api_key: settings['scsb_api_key']})
+    puts "MAPPING of barcodes to customerCodes: #{mapper.barcode_to_customer_code_mapping}"
   end
-
 end
