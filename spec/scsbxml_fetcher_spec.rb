5d95e8db9467d1c89f28cea4be8972a193106c03
Kang Peng <kangtingpeng@nypl.org>
2018-01-19 16:13:13 -0500

    First pass at spec for SCSBXMLFetcher

--- spec/scsbxml_fetcher_spec.rb
+++ spec/scsbxml_fetcher_spec.rb
@@ -4,12 +4,26 @@ describe SCSBXMLFetcher do
   describe "translate_to_scsb_xml" do
 
     it "maps a hash of barcodes => customer_code to a hash of barcodes and the values are SCSBXML Strings" do
-      fake_nypl_bibs_response = double()
-      allow(fake_nypl_bibs_response).to receive(:body) { JSON.generate({'1234' => "NA", "5678" => nil}) }
-      expect(HTTParty).to receive(:post).at_least(:once).and_return(fake_nypl_bibs_response)
+      # Mock OAuth
+      fake_oauth_client = double()
+      fake_oauth_token = double()
+      fake_get_token = double()
+      
 
-      fetcher = SCSBXMLFetcher.new({barcodes: {'1234' => "NA", "5678" => nil}})
-      expect(fetcher.translate_to_scsb_xml).to eq({'1234' => "<?xml version=\"1.0\" ?><bibRecords></bibRecords>", "5678" => nil})
+      allow(fake_get_token).to receive(:token)  {"thisisfakeoauthtoken"}
+      expect(OAuth2::Client).to receive(:new).at_least(:once).and_return(fake_oauth_client)
+      expect(fake_oauth_client).to receive(:client_credentials).at_least(:once).and_return(fake_oauth_token)
+      expect(fake_oauth_token).to receive(:get_token).at_least(:once).and_return(fake_get_token)
+
+      # Mock actual call to nypl-bibs
+      fake_nypl_bibs_response = double()
+      allow(fake_nypl_bibs_response).to receive(:body).at_least(:once) { "<?xml version=\"1.0\" ?><bibRecords></bibRecords>" }
+
+      expect(HTTParty).to receive(:get).at_least(:once).and_return(fake_nypl_bibs_response)
+
+      fetcher = SCSBXMLFetcher.new({barcode_to_customer_code_mapping: {'1234' => "NA", "5678" => nil}})
+
+      expect(fetcher.translate_to_scsb_xml).to eq({'1234' => "<?xml version=\"1.0\" ?><bibRecords></bibRecords>"})
     end
 
     it "returns nil as a value if the barcode or customer_code isn't valid for nypl-bibs API" do
