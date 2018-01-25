1f6d0100229fd730927be8a573c34a86bca78f1c
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-01-25 15:44:59 -0500

    Don't work messages 'younger' than minimum_message_age_seconds

--- lib/message_handler.rb
+++ lib/message_handler.rb
@@ -15,35 +15,43 @@ class MessageHandler
   end
 
   def handle
+    if old_enough?
+      @logger.info "Message body: #{@message.body} with attributes #{@message.attributes} and user_attributes of #{@message.message_attributes}"
+      parsed_message = JSON.parse(@message.body)
+      if parsed_message['action'] && parsed_message['action'] == 'sync'
+        mapper = BarcodeToCustomerCodeMapper.new({barcodes: parsed_message['barcodes'], api_url: @settings['scsb_api_url'], api_key: @settings['scsb_api_key']})
+        mapping = mapper.barcode_to_customer_code_mapping
+        @logger.info "MAPPING of barcodes to customerCodes: #{mapping}"
+        xml_fetcher = SCSBXMLFetcher.new({
+          oauth_key:    @settings['nypl_oauth_key'],
+          oauth_url:    @settings['nypl_oauth_url'],
+          oauth_secret: @settings['nypl_oauth_secret'],
+          platform_api_url: @settings['platform_api_url'],
+          barcode_to_customer_code_mapping: mapping
+        })
+        barcode_to_scsb_xml_mapping = xml_fetcher.translate_to_scsb_xml
+        @logger.info "the barcode to SCSBXML matching is #{barcode_to_scsb_xml_mapping}"
 
-    @logger.info "Message body: #{@message.body} with attributes #{@message.attributes} and user_attributes of #{@message.message_attributes}"
-    parsed_message = JSON.parse(@message.body)
-    if parsed_message['action'] && parsed_message['action'] == 'sync'
-      mapper = BarcodeToCustomerCodeMapper.new({barcodes: parsed_message['barcodes'], api_url: @settings['scsb_api_url'], api_key: @settings['scsb_api_key']})
-      mapping = mapper.barcode_to_customer_code_mapping
-      @logger.info "MAPPING of barcodes to customerCodes: #{mapping}"
-      xml_fetcher = SCSBXMLFetcher.new({
-        oauth_key:    @settings['nypl_oauth_key'],
-        oauth_url:    @settings['nypl_oauth_url'],
-        oauth_secret: @settings['nypl_oauth_secret'],
-        platform_api_url: @settings['platform_api_url'],
-        barcode_to_customer_code_mapping: mapping
-      })
-      barcode_to_scsb_xml_mapping = xml_fetcher.translate_to_scsb_xml
-      @logger.info "the barcode to SCSBXML matching is #{barcode_to_scsb_xml_mapping}"
+        submit_collection_updater = SubmitCollectionUpdater.new(
+            barcode_to_scsb_xml_mapping: barcode_to_scsb_xml_mapping,
+            api_url: @settings['scsb_api_url'],
+            api_key: @settings['scsb_api_key'],
+            is_gcd_protected: false,
+        )
 
-      submit_collection_updater = SubmitCollectionUpdater.new(
-          barcode_to_scsb_xml_mapping: barcode_to_scsb_xml_mapping,
-          api_url: @settings['scsb_api_url'],
-          api_key: @settings['scsb_api_key'],
-          is_gcd_protected: false,
-      )
-
-      submit_collection_updater.update_scsb_items
-
+        submit_collection_updater.update_scsb_items
+      else
+        @logger.error("Not valid action from Simple Queue Service.")
+      end
     else
-      @logger.error("Not valid action from Simple Queue Service.")
+      can_be_processed_at = Time.now.to_i + @settings['minimum_message_age_seconds'].to_i
+      can_be_processed_in = can_be_processed_at - @message.attributes['SentTimestamp']
+      @logger.debug("Message '#{@message.body}' is not old enough to process. It can be processed in #{can_be_processed_in} seconds")
     end
   end
 
+  def old_enough?
+    seconds_since_publishing = Time.now.to_i - @message.attributes['SentTimestamp'].to_i
+    (seconds_since_publishing >= @settings['minimum_message_age_seconds'].to_i)
+  end
 end
--- spec/message_handler_spec.rb
+++ spec/message_handler_spec.rb
@@ -11,7 +11,12 @@ describe MessageHandler do
   end
 
   it "will delete message and log an errror if the message isn't valid JSON"
-  it "will log a 'young' message but not try to process it"
+  it "will log a 'young' message but not try to process it" do
+    fake_message = double(:body => JSON.generate({}), message_attributes: {}, :attributes => {"SentTimestamp" => Time.now.to_i})
+    message_handler = MessageHandler.new({message: fake_message, settings: {'minimum_message_age_seconds' => "300"}})
+    NyplLogFormatter.any_instance.should_receive(:debug).with("Message '{}' is not old enough to process. It can be processed in 300 seconds")
+    message_handler.handle
+  end
 
   describe "handling a message with a value for 'action' that is not in the whitelist" do
     it "will log an error"
