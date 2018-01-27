c7fb22a18ad953c38b1bc8aefc850015e7bf6887
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-01-27 16:43:29 -0500

    Comment on example useage of the Errorable module

--- lib/errorable.rb
+++ lib/errorable.rb
@@ -1,3 +1,10 @@
+# A module used to for classes that, in their duty may generate errors that we want to
+# report to users. For example, an instance of BarcodeToCustomerCodeMapper may
+# fail to connect to or get an unexpected response from a server.
+#
+# To use it `include Errorable` and make sure the constuctor initializes `@errors = {}`
+# .errors() returns a hash where the key is (usually) a barcode and the value is an
+# Array of Strings
 module Errorable
 
   def self.included(base)
