e5c582b5da7b3d5d0bf0d1dbb8e90fd48d379557
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-01-27 21:00:50 -0500

    Copy edit module's description

--- lib/errorable.rb
+++ lib/errorable.rb
@@ -1,5 +1,5 @@
-# A module used to for classes that, in their duty may generate errors that we want to
-# report to users. For example, an instance of BarcodeToCustomerCodeMapper may
+# A module used to for classes that, when performing their duty, can generate errors that
+# users should know about. For example, an instance of BarcodeToCustomerCodeMapper may
 # fail to connect to or get an unexpected response from a server.
 #
 # To use it `include Errorable` and make sure the constuctor initializes `@errors = {}`
