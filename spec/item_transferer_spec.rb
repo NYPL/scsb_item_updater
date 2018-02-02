6fdc320d4f2f84ee227f4591a2aae4f1b6ced03d
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-02-02 11:43:08 -0500

    Add more specs about how ItemTransferer will work.

--- spec/item_transferer_spec.rb
+++ spec/item_transferer_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
 describe ItemTransferer do
-  describe "errors" do
-    it "returns an empty hash before transfer! called" do
+  describe 'making calls' do
+    it 'hits SCSB with the appropriate headers & body'
+  end
+
+  describe 'errors' do
+    it 'returns an empty hash before transfer! called' do
       expect(ItemTransferer.new.errors).to eq({})
     end
+
+    it "parrots the 'error' message from SCSB's response if it exists"
   end
 end
