6ded6da64c5b54b8c9f3239405d3520707038ef6
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-02-09 15:58:39 -0500

    Spec the HTTP call in NyplPlatformClient#fetch_scsbxml_for

--- spec/nypl_platform_client_spec.rb
+++ spec/nypl_platform_client_spec.rb
@@ -1,26 +1,42 @@
 require 'spec_helper'
 
 describe NyplPlatformClient do
+  before do
+    @fake_oauth_client = instance_double('OAuth2::Client', 'client_credentials' => double('get_token' => double('token' => 'myToken')))
+    @client = NyplPlatformClient.new(platform_api_url: "https://example.com")
+  end
+
   describe "refile" do
     it "calls the platform API with expected params" do
-      fake_oauth_client = instance_double('OAuth2::Client', 'client_credentials' => double('get_token' => double('token' => 'myToken')))
-      expect(OAuth2::Client).to receive(:new).at_least(:once).and_return(fake_oauth_client)
-      client = NyplPlatformClient.new(platform_api_url: "https://example.com")
+      expect(OAuth2::Client).to receive(:new).at_least(:once).and_return(@fake_oauth_client)
 
       request_headers = {'Accept' => "application/json", 'Authorization' => 'Bearer myToken'}
-      fake_http_response = double(body: "good job", code: 200)
-
       expect(HTTParty).to receive(:post).at_least(:once).with(
         "https://example.com/api/v0.1/recap/refile-requests",
         headers: request_headers,
         body: JSON.generate({itemBarcode: "a-barcode"})
-      ).and_return(fake_http_response)
+      )
 
-      client.refile('a-barcode')
+      @client.refile('a-barcode')
     end
   end
 
   describe "fetch_scsbxml_for" do
-    it "calls the platform API with expected params"
+    it "calls the platform API with expected params" do
+      expect(OAuth2::Client).to receive(:new).at_least(:once).and_return(@fake_oauth_client)
+
+      expect(HTTParty).to receive(:get).at_least(:once).with(
+        "https://example.com/api/v0.1/recap/nypl-bibs",
+        query: {
+          customerCode: "NA",
+          barcode:      "123",
+          includeFullBibTree: 'false'
+        },
+        headers: {'Authorization' => "Bearer myToken"}
+      )
+
+      @client.fetch_scsbxml_for('123', 'NA')
+  end
+
   end
 end
